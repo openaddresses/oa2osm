608f4e536123256e60ee1e103d0ff773e90d6d7c
Andrew Harvey <andrew@alantgeo.com.au>
2020-04-02 13:53:05 +1100

    add test for input output as arguments and set -e to ensure errors are flagged (#7)
    * add test for input output as arguments and set -e to ensure errors are flagged
    
    * fix closing tag write
    
    * fix closing </osm> tag
    
    * log test numbers
    
    * whitespace
    
    Co-authored-by: Nick <ingalls@protonmail.com>

--- oa2osm.js
+++ oa2osm.js
@@ -100,7 +100,7 @@ pipeline(
     transformer,
     output,
     (err) => {
-        if (argv._.length) {
+        if (argv._.length > 1) {
             fs.appendFileSync(argv._[1], '</osm>\n');
         } else {
             process.stdout.write('</osm>\n');
--- test.sh
+++ test.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+# immediately exit script when it encounters an error
+set -e
+
+# tests with stdin/stdout
+echo "Test 1"
 ./oa2osm.js < test/input.csv | diff - test/output.osm
+
+echo "Test 2"
 ./oa2osm.js --title-case 'CITY' < test/input.csv | diff - test/output-title-case.osm
+
+echo "Test 3"
 ./oa2osm.js --map-city='addr:suburb' < test/input.csv | diff - test/output-map.osm
+
+# test input and output arguments
+echo "Test 4"
+./oa2osm.js test/input.csv | diff - test/output.osm
+
+echo "Test 5"
+./oa2osm.js test/input.csv test/output-result.osm && diff test/output-result.osm test/output.osm
+rm -f test/output-result.osm
